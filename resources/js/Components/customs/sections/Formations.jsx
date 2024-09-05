7e050a41ae6b434582d73a013c84cf39be6fc4e4
ValNeff67 <neffvalentin67@gmail.com>
2024-09-05 08:41:43 +0200

    correct bug

--- resources/js/Components/customs/sections/Formations.jsx
+++ resources/js/Components/customs/sections/Formations.jsx
@@ -17,7 +17,7 @@ export const Formations = () => {
                 <p className='w-3/4 md:w-1/2 mx-auto md:p-[1rem] md:mx-0 text-white border border-transparent pt-4'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce massa sem, pharetra eget lorem quis, ullamcorper egestas turpis. Mauris sit amet lectus bibendum metus dictum rhoncus. Nulla eget purus non lectus venenatis posuere. Nam mattis, urna sit amet congue aliquam, nisi justo pellentesque est, vitae venenatis orci elit ut dolor. Integer nec mattis lectus. Nullam metus erat, viverra sit amet placerat eu, elementum quis quam. Phasellus aliquet auctor tempus. Donec id massa nunc. Praesent at urna velit. Aenean euismod blandit sollicitudin. Sed volutpat purus quis laoreet lobortis.
                 </p>
-                <img src={Corgi} className='w-3/4 mx-auto scale-125 lg:scale-100 md:w-2/5 lg:w-1/4 hidden md:block' />
+                <img src={Corgi} className='w-3/4 mx-auto md:w-2/5 lg:w-1/4' />
             </div>
             <hr className='w-3/4 mx-auto my-3 md:hidden mt-6' />
             <div className='flex flex-col md:justify-between md:flex-row-reverse'>
--- resources/js/Components/customs/sections/Techno.jsx
+++ resources/js/Components/customs/sections/Techno.jsx
@@ -6,9 +6,6 @@ export const Techno = () => {
             <h3 className='text-center mb-3 uppercase font-bold text-lg p-6'>
                 Quelques réalisations
             </h3>
-            <p>
-                
-            </p>
         </article>
     )
 }
