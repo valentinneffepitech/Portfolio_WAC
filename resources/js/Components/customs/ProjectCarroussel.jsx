9f31ccbed667d359fd93725771e1503fc59dfce1
ValNeff67 <neffvalentin67@gmail.com>
2024-09-26 21:46:11 +0200

    update style

--- resources/js/Components/customs/ProjectCarroussel.jsx
+++ resources/js/Components/customs/ProjectCarroussel.jsx
@@ -39,7 +39,7 @@ export const ProjectCarroussel = ({
                 {
                     projects[screen] &&
                     <div className='bg-white shrink-0 w-full md:w-3/4 md:mx-auto lg:w-full lg:flex pb-10 lg:pb-0'>
-                        <img src={projects[screen].image} className='w-full cursor-pointer mb-2 lg:mb-0 aspect-[4/3] border-b-gray-300 border-b-[1px] lg:border-b-0' />
+                        <img src={projects[screen].image} className='w-full cursor-pointer mb-2 lg:mb-0 aspect-[4/3] lg:aspect-auto border-b-gray-300 border-b-[1px] lg:border-b-0' />
                         <div className={`overflow-hidden transition-all duration-200 mt-3 pb-3 lg:w-full lg:flex lg:flex-col lg:p-6 lg:justify-between p-4 lg:border-l-2 lg:border-l-gray-400 lg:my-6`}>
                             <h4 className='underline text-bold underline-offset-2 text-xl my-2 lg:mt-0'>
                                 {projects[screen].name}
@@ -50,8 +50,8 @@ export const ProjectCarroussel = ({
                             <ul className='flex items-center justify-around mt-[1rem]'>
                                 {
                                     projects[screen].technologies.map(techno => (
-                                        <li key={techno.id} className='w-1/5'>
-                                            <img src={techno.image} className='w-full aspect-[4/3]' />
+                                        <li key={techno.id} className='w-1/5 lg:w-[10%]'>
+                                            <img src={techno.image} className='w-full aspect-[4/3] lg:aspect-square' />
                                         </li>
                                     ))
                                 }
--- resources/js/Components/customs/sections/Techno.jsx
+++ resources/js/Components/customs/sections/Techno.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Techno = () => {
     return (
-        <h3 className='text-2xl text-center mb-3 uppercase font-bold p-6 text-white'>
+        <h3 className='text-2xl text-center mb-3 uppercase font-bold p-6 mt-6 text-white'>
             Quelques réalisations
         </h3>
     )
