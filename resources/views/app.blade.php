a8e8c7fc41e0629baf10d1e7a9e0e3ffc3cf771f
valentinneffepitech <valentin.neff@epitech.eu>
2024-10-23 11:47:04 +0200

    update lighthouse

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel="icon" href="/shield.svg">
+        <link rel="icon" href="/mylogo.svg">
         <!-- Scripts -->
         @routes
         @viteReactRefresh
