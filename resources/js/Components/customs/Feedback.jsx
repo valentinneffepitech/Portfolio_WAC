023b9587f72d4709797af60c67d9ef3ed41e2a5b
valentinneffepitech <valentin.neff@epitech.eu>
2024-10-27 21:56:47 +0100

    resolve bug feedback

--- resources/js/Components/customs/Feedback.jsx
+++ resources/js/Components/customs/Feedback.jsx
@@ -4,7 +4,7 @@ export const Feedback = ({
     feedback
 }) => {
     return (
-        <div className='w-[90%] mx-auto py-4 border-t-2 border-b-2 border-collapse md:mt-4 md:w-2/5 md:flex md:flex-col md:justify-between lg:w-[33%] lg:mx-3 lg:max-h-[40vh]'>
+        <div className='w-[90%] mx-auto py-4 border-t-2 border-b-2 border-collapse md:mt-4 md:w-2/5 md:flex md:flex-col md:justify-between lg:w-[30%] lg:mx-3 lg:max-h-[40vh]'>
             <p className='md:my-[1rem]'>
                 {
                     feedback.content
