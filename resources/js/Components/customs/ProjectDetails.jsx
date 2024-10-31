0feb36d580d3c5ad20cd04c9f530031ea17c3047
valentinneffepitech <valentin.neff@epitech.eu>
2024-10-31 22:22:48 +0100

    debeug style responsive

--- resources/js/Components/customs/ProjectDetails.jsx
+++ resources/js/Components/customs/ProjectDetails.jsx
@@ -6,9 +6,9 @@ export const ProjectDetails = ({
     choose
 }) => {
     return (
-        <div className='mb-5 mx-2 w-full md:w-1/3 lg:w-[30%] project_detail transition-all duration-200 rounded pb-4 fadeIn-1 border-b-2' onClick={() => choose(index)}>
+        <div className='mb-12 mx-2 w-full md:w-1/3 lg:w-[30%] project_detail transition-all duration-200 rounded pb-4 fadeIn-1 border-b-2' onClick={() => choose(index)}>
             <div className='w-full mx-auto overflow-hidden flex flex-col'>
-                <img src={project.image} className='h-full cursor-pointer w-full lg:h-[30vh] border border-transparent hover:border-white transition-all duration-300' alt={project.name}/>
+                <img src={project.image} className='h-full cursor-pointer w-full lg:h-[30vh] border-2 border-transparent hover:border-white transition-all duration-300' alt={project.name}/>
                 <div className='w-full flex gap-2 mt-4'>
                     {
                         project.technologies.length != 0 && project.technologies.map(p=>{
